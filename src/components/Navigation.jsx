eb76783222fab08615d533666baf0ec8553ac5be
Vinayak Mandare <85034333+Vinayakmandare@users.noreply.github.com>
2024-11-18 16:32:08 +0530

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../../src/style.css'
 
-const Navigation = () => (
-  <nav className='navbar  bg-light'>
-    <ul>
-      <li><Link to="/">Student List</Link></li>
-      <li><Link to="/register">Register Student</Link></li>
-    </ul>
-  </nav>
-);
+const Navigation = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Student Management</Link>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Student List</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/register">Register Student</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navigation;
+
